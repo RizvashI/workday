6316ee3f15c85a19726c22d86b0681328c30f3af
rizvash_portfolio <rizvash.i+portfolio@gmail.com>
2025-07-24 17:42:09 +0300

    prehook

--- infra/main.tf
+++ infra/main.tf
@@ -88,10 +88,6 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
   user_data                   = file("${path.module}/userdata.sh")
-
-  # 🔑 Если используешь ключ для SSH:
-  # key_name = "your-keypair-name"
-
   tags = {
     Name = "bulls-web"
   }
